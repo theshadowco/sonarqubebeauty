5c75cdeaece629a2cdd1247d253553ed4b09a1c6
Maxmov Valery <maximovvalery@gmail.com>
2018-06-04 09:54:14 +0300

    Скорректирована поседовательность реплейсов

--- src/sonarqubeb.user.js
+++ src/sonarqubeb.user.js
@@ -8,55 +8,57 @@
 // @include http://sonar.silverbulleters.org/*
 // ==/UserScript==
 (function (window, undefined) {
-    var w;
-    if (typeof unsafeWindow != undefined) {
-        w = unsafeWindow
-    } else {
-        w = window;
-    }
+	var w;
+	if (typeof unsafeWindow != undefined) {
+		w = unsafeWindow
+	} else {
+		w = window;
+	}
 
-    if (w.self != w.top) {
-        return;
-    }
+	if (w.self != w.top) {
+		return;
+	}
 
-    var linkDict = [
-        "a.link-no-underline > span"
-        // "a.link-with-icon > span"
-        // "td.code-name-cell > span"
-    ]
+	var linkDict = [
+		"a.link-no-underline > span"
+		// "a.link-with-icon > span"
+		// "td.code-name-cell > span"
+	]
 
-    var replaceDict = {
-        'src/configuration/': '',
-        '/Ext/Form/': '.',
-        '/Forms/': '.',
-        '/Ext/': '.',
-        '/': '.',
-        '.bsl': '',
-        '.ObjectModule': '.МодульОбъекта',
-        '.Module': '.Модуль',
-        '.DataProcessors.': '.Обработки.',
-        '/DataProcessors/': '/Обработки/',
-        '.CommonModules.': '.ОбщиеМодули.',
-        '/CommonModules/': '/ОбщиеМодули/',
-        '.Catalogs.': '.Справочники.',
-        '/Catalogs/': '/Справочники/'
-    }
+	var replaceDict = {
+		'.ObjectModule': '.МодульОбъекта',
+		'.CommandModule': '.МодульКоманды',
+		'.ManagerModule': '.МодульМенеджера',
+		'.Module': '.Модуль',
+		'DataProcessors.': 'Обработки.',
+		'CommonModules.': 'ОбщиеМодули.',
+		'CommonForms.': 'ОбщиеФормы.',
+		'Catalogs.': 'Справочники.'
+		
+	}
 
-    setInterval(function () {
-        for (var key of linkDict) {
-            var elems = window.document.querySelectorAll(key);
-            // var elems = window.document.querySelectorAll('a.link-no-underline > span');
-            for (var i = 0; i < elems.length; i++) {
-                var newText = elems[i].title;
-                for (var key in replaceDict) {
-                    newText = newText.replace(key, replaceDict[key]);
-                }
+	setInterval(function () {
+		for (var key of linkDict) {
+			var elems = window.document.querySelectorAll(key);
+			for (var i = 0; i < elems.length; i++) {
+				
+				// для соблюдения последовательности
+				var newText = elems[i].title.replace('src/configuration/', '')
+					.replace('/Ext/Form/Module', '.МодульФормы')
+					.replace('/Forms/', '.')
+					.replace('/Commands/', '.')
+					.replace('/Ext/', '.')
+					.replace('/', '.')
+					.replace('.bsl', '');
+				
+				// патч по словарю
+				for (var keyDict in replaceDict) {
+					newText = newText.replace(keyDict, replaceDict[keyDict]);
+				}
 
-                elems[i].textContent = newText;
-
-            }
-
-        }
-    }, 300);
+				elems[i].textContent = newText;
+			}
+		}
+	}, 300);
 
 })(window);
