9cee4329ec263eedf0ead3d42e835650f052eb1d
Artur Ayukhanov <aartbear@gmail.com>
2018-06-04 08:46:18 +0300

    Добавил преобразование к привычным коротким русским именам (Модуль, МодульОбъекта) и именам типов метаданных (#2)
    Добавил сервер Сонара sonar.silverbulleters.org
    Отрефакторил код

--- src/sonarqubeb.user.js
+++ src/sonarqubeb.user.js
@@ -3,26 +3,60 @@
 // @description Скрипт для SonarQube для улучшения отображения информации
 // @author Maximov Valery aka theshadowco
 // @license MIT
-// @version 1.0
+// @version 1.1
 // @include https://www.sonarqube.org/*
+// @include http://sonar.silverbulleters.org/*
 // ==/UserScript==
-(function (window, undefined) {  
+(function (window, undefined) {
     var w;
     if (typeof unsafeWindow != undefined) {
         w = unsafeWindow
     } else {
         w = window;
     }
-    
-	if (w.self != w.top) {
+
+    if (w.self != w.top) {
         return;
     }
-    setInterval(function() {
-	var elems = window.document.querySelectorAll('a.link-no-underline > span');
-	for (var i = 0; i < elems.length; i++) {
-		var newText =  elems[i].title.replace('src/configuration/', '').replace('/Ext/Form/', '.').replace('/Forms/', '.').replace('/Ext/', '.').replace('/', '.').replace('.bsl', '');
-		elems[i].textContent = newText; 
-		
-	}}, 300);	    
+
+    var linkDict = [
+        "a.link-no-underline > span"
+        // "a.link-with-icon > span"
+        // "td.code-name-cell > span"
+    ]
+
+    var replaceDict = {
+        'src/configuration/': '',
+        '/Ext/Form/': '.',
+        '/Forms/': '.',
+        '/Ext/': '.',
+        '/': '.',
+        '.bsl': '',
+        '.ObjectModule': '.МодульОбъекта',
+        '.Module': '.Модуль',
+        '.DataProcessors.': '.Обработки.',
+        '/DataProcessors/': '/Обработки/',
+        '.CommonModules.': '.ОбщиеМодули.',
+        '/CommonModules/': '/ОбщиеМодули/',
+        '.Catalogs.': '.Справочники.',
+        '/Catalogs/': '/Справочники/'
+    }
+
+    setInterval(function () {
+        for (var key of linkDict) {
+            var elems = window.document.querySelectorAll(key);
+            // var elems = window.document.querySelectorAll('a.link-no-underline > span');
+            for (var i = 0; i < elems.length; i++) {
+                var newText = elems[i].title;
+                for (var key in replaceDict) {
+                    newText = newText.replace(key, replaceDict[key]);
+                }
+
+                elems[i].textContent = newText;
+
+            }
+
+        }
+    }, 300);
 
 })(window);
